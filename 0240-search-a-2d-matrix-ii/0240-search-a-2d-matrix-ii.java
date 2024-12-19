eb1b2fa0a86f0071adffce311cee86233a0e0773
dileepkumarsasanapuri <127666354+dileepkumarsasanapuri@users.noreply.github.com>
2024-12-19 22:49:08 +0530

    Time: 5 ms (99.89%), Space: 46.1 MB (35.63%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,23 +1,38 @@
 class Solution {
-    static boolean binarysearch(int []arr,int target){
-        int low=0,high=arr.length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==target) 
+    // static boolean binarysearch(int []arr,int target){
+    //     int low=0,high=arr.length-1;
+    //     while(low<=high){
+    //         int mid=(low+high)/2;
+    //         if(arr[mid]==target) 
+    //            return true;
+    //         else if(arr[mid]<target)
+    //            low=mid+1;
+    //         else 
+    //            high=mid-1;
+    //     }
+    //     return false;
+    // }
+    // public boolean searchMatrix(int[][] matrix, int target) {
+    //     for(int i=0;i<matrix.length;i++){
+    //         if(binarysearch(matrix[i],target)){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+
+    
+     public boolean searchMatrix(int[][] matrix, int target){
+        int row=0,col=matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]==target)
                return true;
-            else if(arr[mid]<target)
-               low=mid+1;
+            else if(matrix[row][col]<target)
+               row++;
             else 
-               high=mid-1;
+               col--;
         }
         return false;
-    }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            if(binarysearch(matrix[i],target)){
-                return true;
-            }
-        }
-        return false;
-    }
+     }
 }
