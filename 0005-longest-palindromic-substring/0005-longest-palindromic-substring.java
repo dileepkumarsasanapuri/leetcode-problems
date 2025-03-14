660f7bddd7c30f403f93d35ef2af29a235935deb
Dileep Kumar Sasanapuri <127666354+dileepkumarsasanapuri@users.noreply.github.com>
2025-03-14 23:18:08 +0530

    Time: 2548 ms (5.01%), Space: 45.4 MB (30.33%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -9,17 +9,17 @@ class Solution {
         return true;
     }
     public String longestPalindrome(String s) {
-        if(s.length()<=1) return s;
-        int maxLen=1;
-        String maxStr=s.substring(0,1);
-        for(int i=0;i<s.length();i++){
-            for(int j=i+maxLen;j<=s.length();j++){
-                if(j-i>maxLen && isPalindrome(s.substring(i,j))){
-                    maxLen=j-i;
-                    maxStr=s.substring(i,j);
-                }
+       if(s==null || s.length()<2) return s;
+       String longest="";
+       for(int i=0;i<s.length();i++){
+        for(int j=i;j<s.length();j++){
+            String str=s.substring(i,j+1);
+            if(isPalindrome(str) && str.length()>longest.length())
+            {
+                longest=str;
             }
         }
-        return  maxStr;
+       }
+       return longest;
     }
 }
