ce8a136c6c164115d9183bd695c84a149d32175f
Dileep Kumar Sasanapuri <127666354+dileepkumarsasanapuri@users.noreply.github.com>
2025-03-14 18:11:11 +0530

    Time: 1 ms (92.84%), Space: 44.7 MB (31.36%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,50 +1,23 @@
 class Solution {
-     private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
-    }
-    private void helper(int[] nums, int ind, List<List<Integer>> res) {
-        if(ind == nums.length) {
-            ArrayList<Integer> t = new ArrayList<>();
-            for(int num: nums) t.add(num);
-            res.add(t);
-            return;
-        }
-        for(int i=ind;i<nums.length;i++) {
-            swap(nums, i, ind);
-            helper(nums, ind+1, res);
-            swap(nums, i, ind);
-        }
-    }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        helper(nums, 0, res);
+        List<List<Integer>> res=new ArrayList<>();
+        boolean []freq=new boolean[nums.length];
+        helper(nums,freq,res, new ArrayList<>());
         return res;
     }
+    public void helper(int []nums,boolean []freq,List<List<Integer>> res,ArrayList<Integer> ds){
+        if(ds.size()==nums.length){
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!freq[i]){
+                freq[i]=true;
+                ds.add(nums[i]);
+                helper(nums,freq,res,ds);
+                ds.remove(ds.size()-1);
+                freq[i]=false;
+            }
+        }
+    }
 }
-    // public List<List<Integer>> permute(int[] nums) {
-    //     List<List<Integer>> res = new ArrayList<>();
-    //     boolean freq[]=new boolean[nums.length];
-    //     getpermutations(nums,freq,res,new ArrayList<>());
-    //     return res;
-    // }
-    // static void getpermutations(int []arr,boolean []freq,List<List<Integer>> ans,ArrayList<Integer> ds){
-    //     if(ds.size()==arr.length){
-    //         ans.add(new ArrayList<>(ds));
-    //         return;
-    //     }
-    //     for(int i=0;i<arr.length;i++){
-    //         if(!freq[i]){
-    //             freq[i]=true;
-    //             ds.add(arr[i]);
-    //             getpermutations(arr,freq,ans,ds);
-    //             ds.remove(ds.size()-1);
-    //             freq[i]=false;
-    //         }
-
-    //     }
-    // }
-
-
-
