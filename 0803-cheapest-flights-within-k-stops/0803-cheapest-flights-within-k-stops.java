d24c7ebaeb71c58938c1ec35454949fa126227a6
Dileep Kumar Sasanapuri <127666354+dileepkumarsasanapuri@users.noreply.github.com>
2025-06-06 21:50:51 +0530

    Time: 6 ms (83.26%), Space: 44.4 MB (58.19%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,58 +1,50 @@
 class Solution {
-    class Pair {
-        int f, s;
-        Pair(int f, int s) {
-            this.f = f;
-            this.s = s;
+    class Pair{
+        int f;int s;
+        public Pair(int f,int s){
+            this.f=f;
+            this.s=s;
         }
     }
-    class Tuple {
-        int stops, node, cost;
-        Tuple(int stops, int node, int cost) {
-            this.stops = stops;
-            this.node = node;
-            this.cost = cost;
+    class Tuple{
+        int f;int s;int t;
+        public Tuple(int f,int s,int t){
+            this.f=f;
+            this.s=s;
+            this.t=t;
         }
     }
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-
-        for (int[] flight : flights) {
-            adj.get(flight[0]).add(new Pair(flight[1], flight[2]));
+        List<List<Pair>> adj=new ArrayList<>();
+       
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
-
-        Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(0, src, 0));
-
-        // Track best cost per (node, stops) to avoid revisits
-        int[][] costWithStops = new int[n][k + 2];
-        for (int[] row : costWithStops) Arrays.fill(row, Integer.MAX_VALUE);
-        costWithStops[src][0] = 0;
-
-        while (!q.isEmpty()) {
-            Tuple tp = q.poll();
-            int stops = tp.stops;
-            int node = tp.node;
-            int cost = tp.cost;
-
-            if (stops > k) continue;
-
-            for (Pair neighbor : adj.get(node)) {
-                int nextNode = neighbor.f;
-                int nextCost = neighbor.s;
-                if (cost + nextCost < costWithStops[nextNode][stops + 1]) {
-                    costWithStops[nextNode][stops + 1] = cost + nextCost;
-                    q.add(new Tuple(stops + 1, nextNode, cost + nextCost));
+        for(int []flight:flights){
+            adj.get(flight[0]).add(new Pair(flight[1],flight[2]));
+        }
+        int []dist=new int[n];
+        
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
+        Queue<Tuple> q=new LinkedList<>();
+        q.add(new Tuple(0,src,0));
+        while(!q.isEmpty()){
+            Tuple tp=q.poll();
+            int stops=tp.f;
+            int node=tp.s;
+            int dis=tp.t;
+            if(stops>k) continue;
+            for(Pair pr:adj.get(node)){
+                int nei=pr.f;
+                int wt=pr.s;
+                if(dis+wt<dist[nei] && stops<=k){
+                    dist[nei]=dis+wt;
+                    q.add(new Tuple(stops+1,nei,dis+wt));
                 }
             }
         }
-
-        int minCost = Integer.MAX_VALUE;
-        for (int i = 0; i <= k + 1; i++) {
-            minCost = Math.min(minCost, costWithStops[dst][i]);
-        }
-        return minCost == Integer.MAX_VALUE ? -1 : minCost;
+        if(dist[dst]==Integer.MAX_VALUE) return -1;
+        else return dist[dst];
     }
 }
