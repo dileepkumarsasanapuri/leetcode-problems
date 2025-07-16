71891e48b7eeacee6a56b53cd517105c316689e3
Dileep Kumar Sasanapuri <dileepkumarsasanapuri@gmail.com>
2025-07-16 11:16:06 +0530

    Time: 2 ms (100%), Space: 44.8 MB (68.44%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,24 +1,23 @@
 class Solution {
     public int romanToInt(String s) {
-        int []values=new int[26];
-        values['I' - 'A'] = 1;
-        values['V' - 'A'] = 5;
-        values['X' - 'A'] = 10;
-        values['L' - 'A'] = 50;
-        values['C' - 'A'] = 100;
-        values['D' - 'A'] = 500;
-        values['M' - 'A'] = 1000;
-
-        int ans=0,current=0,prev=0;
+        int[] values=new int[26];
+        values['I'-'A']=1;
+        values['V'-'A']=5;
+        values['X'-'A']=10;
+        values['L'-'A']=50;
+        values['C'-'A']=100;
+        values['D'-'A']=500;
+        values['M'-'A']=1000;
+        int ans=0,cur=0,prev=0;
         for(int i=s.length()-1;i>=0;i--){
-            current=values[s.charAt(i)-'A'];
-            if(current<prev){
-                ans -=current;
+            cur=values[s.charAt(i)-'A'];
+            if(cur<prev){
+                ans -=cur;
             }
             else{
-                ans+=current;
+                ans+=cur;
             }
-             prev=current;
+            prev=cur;
         }
         return ans;
     }
