21dce2e29fff1765864d846ad3ae57763eed7ea0
Dileep Kumar Sasanapuri <dileepkumarsasanapuri@gmail.com>
2025-07-28 22:28:48 +0530

    Time: 1 ms (79.9%), Space: 41.5 MB (65.19%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -4,16 +4,15 @@ class Solution {
      String []st=s.split(" ");
      if(pat.length!=st.length) return false;
      Map<Character,String> map=new HashMap<>();
-     Set<String> set =new HashSet<>();
+    
      for(int i=0;i<pat.length;i++){
         char cur=pat[i];
         String trr=st[i];
         if(map.containsKey(cur)){
             if(!map.get(cur).equals(trr)) return false;
         }
-        else if(set.contains(trr)) return false;
+        else if(map.containsValue(trr)) return false;
         map.put(cur,trr);
-        set.add(trr);
      }
      return true;
     }
