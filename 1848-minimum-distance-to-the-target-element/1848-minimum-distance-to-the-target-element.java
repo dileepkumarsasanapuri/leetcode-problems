0aab107899e0c000a6337aa0dc1fd8099980a327
Dileep Kumar Sasanapuri <dileepkumarsasanapuri@gmail.com>
2026-04-13 10:21:53 +0530

    Time: 1 ms (47.94%), Space: 44.8 MB (69.96%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int res=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                int cur=Math.abs(i-start);
+                res=Math.min(res,cur);
+            }
+        }
+        return res;
+    }
+}
